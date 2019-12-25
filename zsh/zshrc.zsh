fa479c1288984bccfb6776de232bbc257422532f
hackdapp <hackdapp@gmail.com>
2019-12-25 11:41:36 +0800

    feat: iTerm配置

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -6,3 +6,21 @@ source ~/.dotfiles/zsh/functions/fzf-functions.zsh
 source ~/.dotfiles/zsh/functions/git-functions.zsh
 source ~/.dotfiles/zsh/bindings.zsh
 source ~/.dotfiles/zsh/alias.zsh
+
+autoload -U promptinit; promptinit
+
+# optionally define some options
+PURE_CMD_MAX_EXEC_TIME=10
+
+# change the path color
+zstyle :prompt:pure:path color white
+
+# change the color for both `prompt:success` and `prompt:error`
+zstyle ':prompt:pure:prompt:*' color cyan
+
+prompt pure
+
+# added by travis gem
+[ -f /Users/ptun/.travis/travis.sh ] && source /Users/ptun/.travis/travis.sh
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
